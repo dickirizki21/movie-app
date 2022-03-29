62924316b2b2ab83a56c0dd1dcfd24e11c3c83ca
dickirizki21 <dickirizki21@gmail.com>
2022-03-29 11:06:46 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState(' '); 
+
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=641bd26d`;
+
+    const response = await fetch(url);
+    const respponseJson = await response.json();
+
+    if (respponseJson.Search){
+      setMovies(respponseJson.Search);
+    }
+  };
+  
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
+  
+  return (
+    <div className="container-fluid movie-app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+         <MovieListHeading heading="Movies"/>
+         <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+      </div>
+      <div className="row">
+        <MovieList movies={movies} />
+      </div>
+    </div>
+  );
+}; 
+
+export default App;
